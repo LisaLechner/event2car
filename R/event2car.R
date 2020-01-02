50827f3eeeb69b56744827891ade67228567d69f
Lisa Maria Lechner <Lisa.Lechner@uibk.ac.at>
2020-01-02 14:46:34 +0100

    finalize documentation and upload files

--- R/event2car.R
+++ R/event2car.R
@@ -1,37 +1,39 @@
 #' event2car: Calculates \code{CAR} (cumulative abnormal returns) for firm(s) and event(s)
 #'
-#' The function applies the following steps to securities' rates of returns: A)
-#' calculate abnormal returns for \code{estimation_period}(s), B) predict
-#' abnormal returns for event period(s), C) aggregate the predicted abnormal
-#' returns to cumulative abnormal return(s) for \code{event_dates}.
+#' The function calculates abnormal returns and confidence intervals
+#' during the event period(s) as well as
+#' cumulative abnormal returns of event(s).
+#'
 #' The package covers three models for the calculation of the cumulative abnormal returns:
-#' - Mean-Adjusted Model (\code{mean_adj})
-#' - Market-Adjusted Model (\emph{mrkt_adj})
-#' - Within-Sample Marekt-Adjusted Model (\emph{mrkt_adj_within})
+#' - Mean-adjusted model (\code{mean_adj})
+#' - Market-adjusted model using out-of-sample estimation (\emph{mrkt_adj_out})
+#' - Market-adjusted model using within-sample estimation (\emph{mrkt_adj_within})
 #' This is the logic suggested by multiple scholars. See references below.
 #'
 #' The generic function is dispatched for such classes as
 #'  \code{zoo}. (future versions of the package allow for classes of \code{data.frame}.)
-#' If \code{market_model} is \emph{mrkt_adj} or \code{sim}
+#'
+#' If \code{market_model} is \emph{mrkt_adj_out} or \code{mrkt_adj_within}
 #' and \code{regressor} has the length greater than one, the first element of
 #' \code{regressor} will be applied for each security in \code{returns}.
 #'
 #' @param returns an object of \code{data.frame} or \code{zoo} containing rates of returns of securities.
 #' @param regressor an object of the same class as \code{returns} containing regressors.
 #'                  The argument can be omitted, if market model is \code{mean_adj}.
-#' @param event_dates an object of class \code{Date} containing one event date or multiple event dates
+#' @param event_date an character object or an object of class \code{Date} containing one event date or multiple event dates in the format YYYY-MM-DD.
 #' @param estimation_period an object of class \code{intenger} stating the number of days
-#'  prior to the event over which the market model parameters are estimated. Default is 250 days.
+#'  prior to the event over which the market model parameters are estimated. Default is 150 days.
 #'  Note that the event period itself is not included in the event period to prevent the event from influencing the normal performance model parameter estimates.
 #' @param car_lag an object of class \code{intenger} measuring the start of the event window. The default is 1 day prior to the event date.
 #' @param car_lead an object of class \code{intenger} measuring the end of the event window. The default is 5 days after the event date.
-#' @param market_model market_model a character indicating the market model among
-#' \code{mean_adj}, \code{mrkt_adj}, and \code{sim}.
+#' @param method a character indicating the method used to calculate abnormal returns and cumulative abnormal returns. Choose among
+#' \code{mean_adj}, \code{mrkt_adj_out}, and \code{mrkt_adj_within}.
 
-#' @return an object of class \code{data.frame} which contains cumulative abnormal returns, the average cumulative abnormal return
-#' (controls for varying event period durations if non-trading days are in the period), the number of tradingdays,
-#' significance levels of the market-return-coefficient, significance level of the event-dummy-coefficient, and model fit (rsquared) per securities per event date(s).
-#' Note that significance levels and rsquared are NA if market model is \code{mean_adj}.
+#' @return an object of class \code{list} which contains abnormal returns on the event date(s),
+#' confidence intervals of the abnormal returns, and the cumulative abnormal return of the event period(s).
+#' Note that the \code{mean_adj} adn the \code{mrkt_adj_out} method produce mean abnormal returns,
+#' yet the \code{mrkt_adj_within} method delivers coefficients of the event period.
+#'
 #'
 #' @references MacKinlay, A.C. \emph{Event Studies in Economics and Finance}.
 #' Journal of Economic Literature, 35(1):13-39, 1997.
@@ -56,132 +58,181 @@
 #'           event_dates=trumpelection,market_model="mean_adj")
 #' # market adjusted model (out-of sample estimation)
 #' event2car(returns=returns_firms,regressor=return_indx,
-#'           event_dates=trumpelection,market_model="mrkt_adj")
+#'           event_dates=trumpelection,market_model="mrkt_adj_out")
 #' # market adjusted model (within sample estimation)
 #' event2car(returns=returns_firms,regressor=return_indx,
 #'           event_dates=trumpelection,market_model="mrkt_adj_within")
 
 
-#' @export
-event2car <- function(returns=returns,
-                      regressor=regressor,
-                      event_dates = event_dates,
-                      estimation_period= 250,
-                      car_lag = 0,
-                      car_lead = 5,
-                      market_model=c("mean_adj", "mrkt_adj","mrkt_adj_within")){
+event2car <- function(returns = NULL,regressor = NULL,event_date = NULL,
+                      method = c("mean_adj","mrkt_adj_within","mrkt_adj_out"),
+                      imputation = c("mean","none","pmm"),
+                      car_lag = 1,car_lead = 5,estimation_period = 150){
 
-  market_model<- match.arg(market_model)
 
-  car_event <- lapply(seq_along(event_dates),function(ev){
+  # Sanity Checks
+  method <- match.arg(method)
+  imputation <- match.arg(imputation)
 
-    estimation_start<-event_dates[ev]-car_lag-estimation_period
-    estimation_end<-event_dates[ev]-car_lag
-    event_start<-event_dates[ev]-car_lag
-    event_end<-event_dates[ev]+car_lead
+  event_date <- as.Date(event_date)
 
-    car <- lapply(1:ncol(as.data.frame(returns)), function(firm){
-      return <- returns[,firm]
-      if(market_model %in% c("mean_adj")){
-        data <- return}
-      if(market_model %in% c("mrkt_adj","mrkt_adj_within")){
-        data <- merge.zoo(return, regressor,  all = TRUE)}
-      estimation_data <- data[stats::complete.cases(data), ]
+  if (NROW(returns) < estimation_period+car_lag+car_lead) {
+    stop("Length of returns is too short.
+                     Either use a longer returns object or shorten the estimation- or event period.")
+  }
+  if (min(time(returns))>=min(event_date)-estimation_period-car_lag | max(time(returns))<=max(event_date)-car_lead){
+    stop("Length of returns is too short.
+                     Either use a longer returns object or shorten the estimation- or event period.")
+  }
 
-      if(market_model %in% c("mrkt_adj","mean_adj")){
-        estimation_data <- estimation_data[
-          zoo::index(estimation_data) >= estimation_start &
-            zoo::index(estimation_data) <= estimation_end]}
+  if (method %in% c("mrkt_adj_within","mrkt_adj_out")){
+    if (!is.null(ncol(regressor))) {
+      regressor <- regressor[ ,1]
+    }
 
-      if(market_model %in% c("mrkt_adj_within")){
-        estimation_data <- estimation_data[
-          zoo::index(estimation_data) >= estimation_start &
-            zoo::index(estimation_data) <= event_end] }
+    if (NROW(returns) != NROW(regressor)) {
+      stop("Length of returns is not equal to length of regressor")
+    }
+    if (NROW(regressor) < estimation_period+car_lag+car_lead) {
+      stop("Length of regressor is too short.
+                         Either use a longer returns object or shorten the estimation- or event period.")
+    }
+    if (min(time(regressor))>=min(event_date)-estimation_period-car_lag | max(time(regressor))<=max(event_date)-car_lead){
+      stop("Length of regressor is too short.
+                     Either use a longer regressor object or shorten the estimation- or event period.")
+    }
+    if (any(!time(regressor) %in% time(regressor))) {
+      warning("regressor and returns have different time ranges.")
+    }
+  }
 
-      prediction_data <- data[stats::complete.cases(data),]
-      prediction_data <- prediction_data[
-        zoo::index(prediction_data) >= event_start &
-          zoo::index(prediction_data) <= event_end]
 
-      if(length(estimation_data)>0 & length(prediction_data)>0){
-        if(market_model =="mean_adj"){
-          estimation_mean <- mean(estimation_data,na.rm=T)
-          abnormal <- prediction_data[, 1] - estimation_mean
-          tradingdays <- table(!is.na(abnormal))[["TRUE"]]
-          car <- data.frame(car=sum(zoo::zoo(abnormal[,1])),car_ave = sum(zoo::zoo(abnormal[,1]))/tradingdays,tradingdays=tradingdays)
-        }
-
-        if(market_model %in% c("mrkt_adj")){
-
-          y <- zoo::coredata(estimation_data[, 1])
-          x <- zoo::coredata(estimation_data[, 2])
-
-          lm_fit <- stats::lm(y ~ x)
-          if(!is.na(lm_fit$coefficients[2])){
-            predicted <- stats::predict.lm(object = lm_fit,
-                                           newdata = data.frame(x = prediction_data[, 2]),
-                                           interval = c("confidence"), level = 0.95)
-            abnormal <-  prediction_data[, 1] - predicted[, 1]
-            tradingdays <- table(!is.na(abnormal))[["TRUE"]]
-            car <- data.frame(car=sum(zoo::zoo(abnormal[,1])),car_ave =sum(zoo::zoo(abnormal[,1]))/tradingdays,tradingdays=tradingdays ,sign_mrkt=summary(lm_fit)$coefficients[2,4],r=summary(lm_fit)$r.squared)
-          } else{
-            warning('NaN created for CAR: No data during event window or estimation window.')
-            car <- data.frame(car=NaN,car_ave=NA,tradingdays=NA,sign_mrkt=NaN,r=NaN)
-          }
-        }
-
-        if(market_model %in% c("mrkt_adj_within")){
-          estimation_data$event = ifelse(index(estimation_data) %in% event_start:event_end,1,0)
-          if(length(table(estimation_data$event))>1){
-            y <- zoo::coredata(estimation_data[, 1])
-            x <- zoo::coredata(estimation_data[, 2])
-            z <- zoo::coredata(estimation_data[, 3])
-            lm_fit <- stats::lm(y ~ x+z)
-            tradingdays <- nrow(na.omit(subset(subset(estimation_data,time(estimation_data) %in% event_start:event_end))))
-            car <- data.frame(car=lm_fit$coefficients[3],car_ave=lm_fit$coefficients[3],tradingdays=tradingdays,sign_mrkt=summary(lm_fit)$coefficients[2,4],sign_car=summary(lm_fit)$coefficients[3,4],r=summary(lm_fit)$r.squared)
-          }
-          else{
-            warning('NaN created for CAR: Not enough data in estimation and/or prediction data.')
-            car <- data.frame(car=NaN,car_ave=NA,tradingdays=NA,sign_mrkt=NaN,sign_car=NaN,r=NaN)
-          }
-        }
+  # Handling missing data
+  ## mean imputation
+  if (imputation == "mean") {
+    if (any(colSums(is.na(returns))>0)) {
+      missing <- colnames(returns)[colSums(is.na(returns))>0]
+      warning(paste("Imputed data using mean returns of the following firm(s):",
+                    paste(missing, collapse=' ')))
+    }
+    returns <- zoo::na.aggregate(returns,FUN=mean,na.rm=T)
+  }
+  ## no imputation: dropping firms with NAs
+  if (imputation == "none") {
+    if (any(colSums(is.na(returns))>0)) {
+      missing <- colnames(returns)[colSums(is.na(returns))>0]
+      warning(paste("Drop following firm(s) due to missing data:",
+                    paste(missing, collapse=' ')))
+    }
+    returns <- returns[,colSums(is.na(returns))==0]
+  }
+  ## pmm: predictive mean matching
+  if (imputation == "pmm") {
+    if (library("mice",logical.return=TRUE)) {
+      if (any(colSums(is.na(returns))>0)) {
+        missing <- colnames(returns)[colSums(is.na(returns))>0]
+        warning(paste("Imputed data using predictive mean matching of the following firm(s):",
+                      paste(missing, collapse=' ')))
       }
-      else{
 
-        if(market_model %in% c("mrkt_adj")){
-          warning('NaN created for CAR: No data during event window or estimation window.')
-          car <- data.frame(car=NaN,car_ave=NaN,tradingdays=NaN,sign_mrkt=NaN,r=NaN)
-        }
-        if(market_model %in% c("mrkt_adj_within")){
-          warning('NaN created for CAR: No data during event window or estimation window.')
-          car <- data.frame(car=NaN,car_ave=NaN,tradingdays=NaN,sign_mrkt=NaN,sign_car=NaN,r=NaN)
-        }
-        if(market_model %in% c("mean_adj")){
-          warning('NaN created for CAR: No data during event window or estimation window.')
-          car <- data.frame(car=NaN,car_ave=NaN,tradingdays=NaN)
-        }
 
-      }
-      return(car)})
-    car <- ldply(car)
-    car$firm <- names(returns)
-    return(car)
+     dates_returns <- time(returns)
+     returns <- mice::complete(mice::mice(returns,method="pmm",printFlag = FALSE))
+     row.names(returns) <- dates_returns
+     returns <- zoo::zoo(returns)
+     zoo::index(returns) <- dates_returns
+    } else {
+      stop("'mice' package not installed. Choose another imputation method or run install.packages('mice')")
+    }
+  }
 
+  # Estimate abnormal returns, confidence intervals,
+  # and the cumulative abnormal return for each event and each firm
+  temp <- lapply(event_date,function(event){
+    ## Mean adjusted model
+    if (method == "mean_adj") {
+      ### data prep
+      #### estimation period data
+      y <- window(returns, start=(event-estimation_period-car_lag),end=(event-car_lag))
+      #### event period data
+      y2 <- window(returns, start=(event-car_lag),end=(event+car_lead))
+      ### abnormal returns
+      ar <- sapply(seq_len(ncol(y)),function(i){
+        as.numeric(y2[,i]) - mean(as.numeric(y[,i]))})
+
+      ci <- sapply(seq_len(ncol(ar)),function(i){
+        se <- qnorm(0.975)*sd(ar[,i])/sqrt(nrow(ar))
+        m <- mean(ar[,i])
+        c(m-se, m+se)
+      })
+      ci <- t(ci)
+      ### output
+      out <- cbind(colMeans(ar),ci)
+      colnames(out) <- c("ar_mean","ci_lower","ci_upper")
+
+      out <- data.frame(out)
+      out$car <- colSums(ar)
+      out$firm <- names(y)
+      row.names(out) <- NULL
+
+      out <- out[c(5,1:4)]
+    }
+    ## Market adjusted (within sample) model
+    if (method == "mrkt_adj_within") {
+      ### data prep
+      y <- window(returns, start=(event-estimation_period-car_lag),end=(event+car_lead))
+      x1 <- window(regressor, start=(event-estimation_period-car_lag),end=(event+car_lead))
+      x2 <- time(y) %in% seq(as.Date(event-car_lag), as.Date(event+car_lead), "days")
+      ### regression inkl. event period
+      z <- lm(y~x1+x2)
+      ### output
+      out <- cbind(coef(z)[3,],
+                   confint(z)[rep(c(FALSE,FALSE,TRUE),ncol(y)),])
+      colnames(out) <- c("ar_coef","ci_lower","ci_upper")
+
+      out <- data.frame(out)
+      out$car <- out$ar * sum(ifelse(x2==TRUE,1,0))
+      out$firm <- rownames(out)
+      row.names(out) <- NULL
+
+      out <- out[c(5,1:4)]
+    }
+    ## Market adjusted (within sample) model
+    if (method == "mrkt_adj_out") {
+      ### data prep
+      #### estimation period data
+      y <- window(returns, start=(event-estimation_period-car_lag),end=(event-car_lag))
+      x1 <- window(regressor, start=(event-estimation_period-car_lag),end=(event-car_lag))
+      #### event period data
+      y2 <- window(returns, start=(event-car_lag),end=(event+car_lead))
+      x12 <- window(regressor, start=(event-car_lag),end=(event+car_lead))
+      ### regression
+      z <- lm(y~x1)
+      ### prediction or abnormal returns
+      ar <- sapply(seq_along(x12),function(i){
+        y2[i, ] - (coef(z)[1, ] + coef(z)[2, ] * x12[[i]])
+        })
+      ci <- sapply(seq_len(nrow(ar)),function(i){
+        ar_se <- qnorm(0.975)*sd(ar[i,])/sqrt(ncol(ar))
+        ar_mean <- mean(ar[i,])
+        c(ar_mean-ar_se, ar_mean+ar_se)
+      })
+      ci <- t(ci)
+      ### output
+      out <- cbind(rowMeans(ar),ci)
+      colnames(out) <- c("ar_mean","ci_lower","ci_upper")
+
+      out <- data.frame(out)
+      out$car <- rowSums(ar)
+      out$firm <- rownames(out)
+      row.names(out) <- NULL
+
+      out <- out[c(5,1:4)]
+    }
+    return(out)
   })
-  names(car_event) <- event_dates
-  car_event <- ldply(car_event)
-
-  if(market_model %in% c("mrkt_adj")){
-    names(car_event) <- c("event","car","car_ave","tradingdays","sign_mrkt","rsquared","firm")
-  }
-  if(market_model %in% c("mrkt_adj_within")){
-    names(car_event) <- c("event","car","car_ave","tradingdays","sign_mrkt","sign_car","rsquared","firm")
-  }
-  if(market_model %in% c("mean_adj")){
-    names(car_event) <- c("event","car","car_ave","tradingdays","firm")
-  }
-
-  return(car_event)
+  names(temp) <- event_date
+  return(temp)
 }
 
 
